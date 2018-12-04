db817b73fce6c5f30ee18bb59468135f523d5a13
Chris Metcalf <chris@chrismetcalf.net>
2018-12-03 23:15:44 -0800

    Updated for my needs!

--- envelope.py
+++ envelope.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
+import sys
 import cairo
 import csv
 
@@ -11,18 +12,22 @@ def write_envelopes(out, from_addr, to_addrs):
                                7.25 * INCHES_TO_POINTS,
                                5.25 * INCHES_TO_POINTS)
     cr = cairo.Context(surface)
-    cr.select_font_face('serif')
+    cr.select_font_face('sans-serif',
+            cairo.FONT_SLANT_NORMAL,
+            cairo.FONT_WEIGHT_BOLD)
 
     MARGIN = 0.25
     for to_addr in to_addrs:
         for i, line in enumerate(from_addr):
+            cr.set_font_size(10.0)
             cr.move_to(MARGIN * INCHES_TO_POINTS,
                        (MARGIN * INCHES_TO_POINTS) + 12 + (12 * i))
             cr.show_text(line)
 
         for i, line in enumerate(to_addr):
+            cr.set_font_size(14.0)
             cr.move_to(2.5 * INCHES_TO_POINTS,
-                       (2.25 * INCHES_TO_POINTS) + 12 + (12 * i))
+                       (2.25 * INCHES_TO_POINTS) + 16 + (16 * i))
             cr.show_text(line)
         cr.show_page()
 
@@ -40,19 +45,30 @@ def load_csv(filename):
                 continue
 
             type = ''
-            if len(row) > 3:
-                type = row[3].strip()
-            if type != 'yes':
+            if len(row) > 7:
+                type = row[7].strip()
+            if type.lower() != 'yes':
                 continue
-            yield row[0:3]
+
+            addr = filter(None, [
+                    row[0], # Name
+                    row[1], # Addr Line 1
+                    row[2], # Addr Line 1
+                    row[3] + ", " + row[4] + " " + row[5], # City, State Postal
+                    row[6] # Country
+            ])
+
+            print(addr)
+
+            yield addr
 
 
 if __name__ == '__main__':
-    FROM_ADDR = ('Evan + Meena',
-                 '[elided]',
-                 'San Francisco, CA 94110')
-    INFILE = 'address.csv'
+    FROM_ADDR = ('Wait-Metcalf',
+                 'REDACTED',
+                 'Seattle, WA 98133')
+    INFILE = sys.argv[1]
     OUTFILE = 'out.pdf'
     with open(OUTFILE, 'w') as f:
         write_envelopes(f, FROM_ADDR, list(load_csv(INFILE)))
-    print 'wrote %s.' % OUTFILE
+    print('wrote %s.' % OUTFILE)
